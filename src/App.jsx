fbbb75ea2f3763e3aeaf7055039dbc1a8364132d
Jp Keys <jpmusik29@gmail.com>
2024-03-05 11:34:44 -0500

    Setting up Sidebar

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './index.css';
+import SideBar from './components/Sidebar';
+
+function App() {
+
+  return (
+    <>
+      
+      
+      <header className='header'>
+        <h1>Statistics Review</h1>
+        <p>Click on the Topic of your choice </p>
+      </header>
+      <div>
+        
+      </div>
+      <SideBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+export default function SideBar() {
+    return <aside id="sidebar">
+        <h2>Topics</h2>
+        <menu>
+            <ul><button>Experiment Design</button></ul>
+            <ul><button>Descriptive Statistics</button></ul>
+            <ul><button>Introduction to Probability</button></ul>
+            <ul><button>Probaility</button></ul>
+            <ul><button>Normal Distribution</button></ul>
+        </menu>
+
+    </aside>
+}
