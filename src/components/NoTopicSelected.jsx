5d3ad65f14122cfafda4bfa855891015b789c037
Jp Keys <jpmusik29@gmail.com>
2024-03-11 08:40:29 -0400

    NoTopicSelectd image

--- src/components/NoTopicSelected.jsx
+++ src/components/NoTopicSelected.jsx
@@ -1,9 +1,9 @@
-// import noTopicImage from '../assets/project.jpeg'
+import noTopicImage from '../assets/project.jpeg'
 
 export default function NoTopicSelected() {
     return (
         <div>
-            {/* <img src={noTopicImage} /> */}
+            <img src={noTopicImage} width='500' height='350' />
             <p>Select a topic from the sidebar</p>
         </div>
     )
