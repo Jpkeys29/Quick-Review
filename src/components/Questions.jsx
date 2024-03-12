d9fdad47efe9c5c1e4dbddc10b3796684ec85db2
Jp Keys <jpmusik29@gmail.com>
2024-03-12 09:14:47 -0400

    Outputting questions dynamically

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -3,8 +3,25 @@ import { DESCRIPT, QUEST } from "./questiArray"
 export default function Questions() {
     return (
         <div className="questions">
-            {QUEST.map((question) => <p key={question.id}>{question.title} {question.answer} </p> )}
-            
+            {QUEST.map( question => (
+                <ul key={question.id}>
+                    <li>
+                        {question.title}
+                    </li>
+                    <p>{question.answer} </p>
+                </ul>
+            ))}
+            {DESCRIPT.map(question => (
+                <ul key={question.id}>
+                    <p>
+                        {question.title}
+                    </p>
+                </ul>
+
+            ))}
+
+            {/* {QUEST.map(question => <p key={question.id}>{question.title} {question.answer} </p> )}
+            {DESCRIPT.map(question => <p key={question.id} >{question.title} {question.answer} </p>)}  */}
         </div>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,9 @@
-export default function SideBar({onSelectedQuestion}) {
+export default function SideBar({onSelectedQuestion, question}) {
     return <aside id="sidebar">
         <h2>Topics</h2>
         <menu>
-            <ul><button onClick={onSelectedQuestion}>Experiment Design</button></ul>
-            <ul><button>Descriptive Statistics</button></ul>
+            <ul><button onClick={() => onSelectedQuestion(question) }>Experiment Design</button></ul>
+            <ul><button onClick={() => onSelectedQuestion(question)}>Descriptive Statistics</button></ul>
             <ul><button>Introduction to Probability</button></ul>
             <ul><button>Probaility</button></ul>
             <ul><button>Normal Distribution</button></ul>
