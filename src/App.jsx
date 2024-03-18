1d9530c2be16f9d3b1b987e70099b4161b42aa1f
Jp Keys <jpmusik29@gmail.com>
2024-03-18 08:39:16 -0400

    Styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './index.css';
 import SideBar from './components/Sidebar';
 import Questions from './components/Questions';
 import NoTopicSelected from './components/NoTopicSelected';
+import { QUEST } from './components/questiArray';
 
 function App() {
   const[selectedQuestion, setSelectedQuestion] = useState ({
     selectedQuestionId: undefined,
-    questions: []
+    // questions: []
   })
 
 function handleAddQuestion() {
@@ -24,18 +25,20 @@ let pregunta;
 if (selectedQuestion.selectedQuestionId === null) {
   pregunta = <Questions />
 } else if (selectedQuestion.selectedQuestionId === undefined) {
-  pregunta = <NoTopicSelected onSelectedQuestion={handleAddQuestion} />
+  pregunta = <NoTopicSelected />
 }
 
   return (
-    <main className='container'> 
+    <main> 
       <header className='header'>
         <h1>Statistics Review</h1>
+
         <p>Click on the Topic of your choice </p>
       </header>
-      <SideBar onSelectedQuestion={handleAddQuestion}/>
-      {/* <NoTopicSelected /> */}
-      {pregunta}
+      <div className='container'>
+        <SideBar  onSelectedQuestion={handleAddQuestion} />
+        {pregunta}
+      </div>
     </main>
   )
 }
