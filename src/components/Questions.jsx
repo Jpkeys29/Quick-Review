80d16031c2e4af53d272ddb32266f61e5c5ee20f
Jp Keys <jpmusik29@gmail.com>
2024-03-29 09:26:08 -0400

    Conditionally rendering answers

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react'
 
 export default function Questions({ topicSelected }) {
     const[currentIndex, setCurrentIndex] = useState(0);
+    const[isOpen, setIsOpen] = useState(false);
+
 
     function handleNextQuestion() {
         setCurrentIndex(current => current + 1);
@@ -14,11 +16,17 @@ export default function Questions({ topicSelected }) {
         setCurrentIndex(current => current - 1);
     }
 
+    function handleToggle() {
+        setIsOpen(currentState => !currentState);
+    }
+
     const categoryA = <ul>
-        <h2> {QUEST[currentIndex].title}</h2>
-        <p>{QUEST[currentIndex].answer}</p> 
         <button onClick={handlePrevious} disabled={currentIndex < 1}>Previous</button>
         <button onClick={handleNextQuestion} disabled={currentIndex === QUEST.length - 1} >Next</button>
+        {!isOpen ? <h2> {QUEST[currentIndex].title}</h2>:(
+            <p>{QUEST[currentIndex].answer}</p>)  
+        }
+        <button onClick={handleToggle}>Answer</button>
     </ul>
 
     const categoryB = <ul>
@@ -66,11 +74,5 @@ function Question({ question }) {
             <h2>{question.title}</h2>
             <p>{question.answer} </p>
         </div>
-        // <div>
-        //     <ul>
-        //         <h4>{question.title}</h4>
-        //         <span>{question.answer} </span>
-        //     </ul>
-        // </div>
     )
 }
