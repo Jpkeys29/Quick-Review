61a126a1e799b0a8a8a4b6b76844954d85e8514f
Jp Keys <jpmusik29@gmail.com>
2024-04-12 11:23:36 -0400

    Style cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,35 @@ import SideBar from './components/Sidebar';
 import Questions from './components/Questions';
 import NoTopicSelected from './components/NoTopicSelected';
 import { QUEST } from './components/questiArray';
+import Header from './components/Header';
 
 function App() {
-  const[topicSelected, setTopicSelected] = useState('');
+  const [topicSelected, setTopicSelected] = useState('');
 
   function handleStart() {
     setTopicSelected('');
   }
 
   return (
-    <main > 
-      <Header />  
-      <div className='app'>
-        <SideBar 
-        topicSelected={topicSelected} 
-        setTopicSelected={setTopicSelected}>
-          <p>Topics </p>
-        </SideBar>
-        {topicSelected ? (<Questions 
-        topicSelected={topicSelected} 
-        setTopicSelected={setTopicSelected} 
-       />) : <NoTopicSelected/>} 
-      </div>
-      {topicSelected && <button onClick={handleStart}>Start</button>}
-    </main>
+    <div>
+        <Header />
+      <main >
+        <div className='app'>
+          <SideBar
+            topicSelected={topicSelected}
+            setTopicSelected={setTopicSelected}>
+            <p>Topics </p>
+          </SideBar>
+          {topicSelected ? (<Questions
+            topicSelected={topicSelected}
+            setTopicSelected={setTopicSelected}
+          />) : <NoTopicSelected />}
+        </div>
+        {topicSelected && <button onClick={handleStart}>Start</button>}
+      </main>
+    </div>
   )
 }
 
-
-
-function Header() {
-  return <h1 className='header'>Statistics Review</h1>
-}
 
 export default App
