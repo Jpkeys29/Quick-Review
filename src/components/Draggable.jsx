4e0ab66f95a66ec476eaa4cc1e7aa8c6eb104dd4
Jp Keys <jpmusik29@gmail.com>
2025-05-20 08:28:30 -0400

    Setting up draggable items

--- src/components/Draggable.jsx
+++ src/components/Draggable.jsx
@@ -1,9 +1,27 @@
 export const Draggable = () => {
+    const dragItems = [
+        { id: 2, value: "drag 1"},
+        { id: 1, value: "drag 2"},
+        { id: 3, value: "drag 3"},
+        { id: 4, value: "drag 4"},
+    ]
     return(
-        <div>
-            <div>One</div>
+        <>
+        {
+            dragItems.map(item => (
+                <div
+                key={item.id}
+                id={item.id}
+                className="item"
+                draggable
+                >
+                    {item.value}
+                </div>
+            ))
+        }
+            {/* <div>One</div>
             <div>Two</div>
-            <div>Three</div>
-        </div>
+            <div>Three</div> */}
+        </>
     )
 }
