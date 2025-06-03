588d613077044e21e0c8ca177b6bf79aef7a83d1
Jp Keys <jpmusik29@gmail.com>
2025-06-03 06:29:01 -0400

    SideBar links

--- src/components/SidePage.jsx
+++ src/components/SidePage.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState, useRef } from "react";
 export const SidePage = () => {
     return(
         <div className="sidePage">
-            <div>
-                SidePage
+            <div className="links">
+                <a href="">Link1</a>
+                <a href="">Link2</a>
+                <a href="">Link3</a>
             </div>
         </div>
     )
